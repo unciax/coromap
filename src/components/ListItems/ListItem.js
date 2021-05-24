b17599356076fe91c278246b362967b2a18f9549
Tzuhui <angel1996421@gmail.com>
2021-05-24 22:43:09 +0800

    fix: convert timestamp to date

--- src/components/ListItems/ListItem.js
+++ src/components/ListItems/ListItem.js
@@ -5,7 +5,13 @@ import "./styles.scss";
 
 const ListItem = props => {
     const { data } = props;
-
+    const fillZero = num => {
+        return parseInt(num) > 9? parseInt(num) : `0${parseInt(num)}`;
+    }
+    const dateTransform = datetime => {
+        const date = new Date(datetime * 1000);
+        return `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} ${fillZero(date.getHours())}:${fillZero(date.getMinutes())}:${fillZero(date.getSeconds())}`
+    }
     return(
         <div className="list-item" key={data["shop_name"]}>
             <div><h2>{data["shop_name"]} <Icon onClick={()=>{ window.open(data["Google map url"], "_blank") }}>map</Icon></h2></div>
@@ -15,7 +21,7 @@ const ListItem = props => {
             <p>是否提供外帶: {data["outside"]}</p>
             <p>是否提供外送: {data["delivery"]}</p>
             <p>是否繼續維持營業: {data["open"]}</p>
-            <p>時間戳記: {data["last_updated_at"]}</p>
+            <p>時間戳記: {dateTransform(data["last_updated_at"])}</p>
             <p>營業時間調整情況: {data["open_time_change"]}</p>
             <p>防疫外帶或外送優惠: {data["discount"]}</p>
         </div>
